aa0fea8ef32d2282da9aab6cf6a21368f153ffb9
Arthur Maltson <arthur.maltson@capitalone.com>
2023-12-01 23:34:41 -0500

    feat: solve the first problem
    In the main function open up our input file and solve it.

--- day1/calibration_values.go
+++ day1/calibration_values.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 )
 
@@ -26,4 +29,23 @@ func findFirstNumber(line string) (int, error) {
 
 func main() {
 	fmt.Println("Starting...")
+	file, err := os.Open("input_file.txt")
+	if err != nil {
+		log.Fatal("Couldn't open file: input_file.txt. Please provide an input file.")
+	}
+	defer file.Close()
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		foundNumber, _ := findFirstNumber(line)
+		total += foundNumber
+	}
+
+	fmt.Printf("Grand total: %d\n", total)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Ran into an error with the bufio Scanner")
+	}
 }
